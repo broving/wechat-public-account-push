1160a6fc27e7ec87b337030e2c01d35a32d16f19
broving <119400583+broving@users.noreply.github.com>
2024-02-15 01:36:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,7 +1,16 @@
 /* eslint-disable */
+
+/**
+ * 此项目配置为方便新人使用，已缩减至最简配置。
+ * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+ * 自行添加属性，以支持更多个性化功能
+ */
 const USER_CONFIG = {
 
+  // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx4abcd005b562ff9f',
+
+  // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '76bd0e40871fadb52e31ce5f668fba24',
 
   PROVINCE: '江苏',
@@ -9,12 +18,33 @@ const USER_CONFIG = {
 
   USERS: [
     {
-      ids: 'o-_as6GRDerRVQK3lEEJIjHw0zu4',
-      useTemplateId: 'TtkzHo4_8aZUUGiN5QIl9oDTkJS8OogOEJ3BO67DKfw',    
-      
+      // 想要发送的人的名字
+      name: '宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'o-_as6GRDerRVQK3lEEJIjHw0zu4',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'TtkzHo4_8aZUUGiN5QIl9oDTkJS8OogOEJ3BO67DKfw',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '12-27',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '李四', year: '1996', date: '09-31',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2023-12-05' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
@@ -27,11 +57,10 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '1	A0306	o-_as6GRDerRVQK3lEEJIjHw0zu4',
+      id: '',
     }
   ],
 
 }
 
 module.exports = USER_CONFIG
-
