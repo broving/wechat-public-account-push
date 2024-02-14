a3bf982b5d5d85cb277fd8f57ea196736e2f4353
broving <119400583+broving@users.noreply.github.com>
2024-02-14 23:33:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -18,7 +18,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '吴惠凤',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o-_as6MlbZiv_iGAJc3YoXIiZihU',
+      id: ['o-_as6MlbZiv_iGAJc3YoXIiZihU','o-_as6GRDerRVQK3lEEJIjHw0zu4'],
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'xfZtPb_4kLkFolEPMGKaB5OO89xvLffZ9hL03bdNeF0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
